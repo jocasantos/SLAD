e422fffe76d1accb6dfad29575c91717b1dc0101
jocasantos <146140765+jocasantos@users.noreply.github.com>
2023-09-26 23:19:57 +0100

    Add files via upload

---
+++ SLAD/data/results.js
@@ -0,0 +1,42 @@
+const results = [{
+    date: 'Sáb 20 Set, 15:30H',
+    logo1: 'data/images/slad-logo-removebg.png',
+    logo2: 'data/images/gdm-aaubi-logo-removebg.png',
+    age: 'Infantis',
+    team1: '<b>SLAD</b>',
+    team2: 'Adversário',
+    competion: 'Camp. Distrital',
+    score1: '<b>4</b>',
+    score2: 2
+}, {
+date: 'Dom 21 Set, 10H',
+logo1: 'data/images/slad-logo-removebg.png',
+logo2: 'data/images/gdm-aaubi-logo-removebg.png',
+age: 'Iniciados',
+team1: 'SLAD',
+team2: '<b>GDM/AAUBI</b>',
+competion: 'Taça Distrital',
+score1: 4,
+score2: '<b>5</b>'
+}, {
+    date: 'Sáb 27 Set, 11H',
+    logo1: 'data/images/slad-logo-removebg.png',
+    logo2: 'data/images/gdm-aaubi-logo-removebg.png',
+    age: 'Juvenis',
+    team1: 'SLAD',
+    team2: 'GDM/AAUBI',
+    competion: 'Camp. Distrital',
+    score1: '-',
+    score2: '-'
+    }, {
+        date: 'Dom 28 Set, 16H',
+        logo1: 'data/images/slad-logo-removebg.png',
+        logo2: 'data/images/gdm-aaubi-logo-removebg.png',
+        age: 'Júniores',
+        team1: 'SLAD',
+        team2: 'GDM/AAUBI',
+        competion: 'Camp. Distrital',
+        score1: '-',
+        score2: '-'
+        }
+];
