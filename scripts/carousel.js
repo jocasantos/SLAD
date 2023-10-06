8d91eb38fcb671dd9a70ae90269b901176e8ebf7
jocasantos <146140765+jocasantos@users.noreply.github.com>
2023-10-06 17:10:50 +0100

    Add files via upload

---
+++ scripts/carousel.js
@@ -0,0 +1,83 @@
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const previousButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// ARRANGE THE SLIDES NEXT TO ONE ANOTHER
+function setSlidePosition (slide, index) {
+    slide.style.left = slideWidth * index + 'px';
+};
+slides.forEach(setSlidePosition);
+
+function moveToSlide (track, currentSlide, targetSlide) {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide'); 
+};
+
+function updateDots (currentDot, targetDot) {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+};
+
+function hideShowArrows (slides, previousButton, nextButton, targetIndex) {
+    if (targetIndex === 0) {
+        previousButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length - 1) {
+        previousButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        previousButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden'); 
+    };
+};
+
+
+
+// WHEN I CLICK LEFT, MOVE SLIDES TO THE LEFT
+previousButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const previousSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const previousDot = currentDot.previousElementSibling;
+    const previousIndex = slides.findIndex(slide => slide === previousSlide);
+
+    moveToSlide(track, currentSlide, previousSlide);
+    updateDots(currentDot, previousDot);
+    hideShowArrows(slides, previousButton, nextButton, previousIndex);
+});
+
+// WHEN I CLICK RIGHT, MOVE SLIDES TO THE RIGHT
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+    moveToSlide (track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, previousButton, nextButton, nextIndex);
+});
+
+// WHEN I CLICK THE NAV INDICATORS, MOVE TO THAT SLIDE
+dotsNav.addEventListener('click', e => {
+    // what indicator was click on?
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, previousButton, nextButton, targetIndex);
+});
